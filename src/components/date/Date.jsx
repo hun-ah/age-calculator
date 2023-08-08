2fdc2c4554145a4549bc31ebd3c77edf9c1e47a1
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-08 10:33:23 -0400

    added logic for displaying numbers and text

--- src/components/date/Date.jsx
+++ src/components/date/Date.jsx
@@ -1,10 +1,13 @@
 import styles from './date.module.css';
 
 const Date = ({ text, number }) => {
+  const display = number || number === 0 ? number : '- -';
   return (
     <div className={styles.container}>
-      <span className={styles.number}>{number ? number : '- -'}</span>
-      <span className={styles.text}>{text}</span>
+      <span className={styles.number}>{display}</span>
+      <span className={styles.text}>
+        {number === 1 ? text.slice(0, -1) : text}
+      </span>
     </div>
   );
 };
